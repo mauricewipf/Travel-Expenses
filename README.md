ace583095d4dd93d988ad5ba7611820af14c30d0
Maurice Wipf <maurice@gmx.de>
2018-01-14 10:22:58 +0100

    1st commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# Travel Expenses
+
+- A web app built on [Oracle JET](http://www.oracle.com/webfolder/technetwork/jet/index.html)
+
+## Run locally
+
+```
+$ npm install -g @oracle/ojet-cli
+$ cd Travel-Expenses
+$ npm install
+$ ojet serve
+```
---
+++ src/js/jet-composites/expense-input/viewModel.js
@@ -0,0 +1,23 @@
+define(['knockout'],
+  function (ko) {
+    function model (context) {
+      var self = this;
+
+    context.props.then(function(properties) {
+    });
+
+    self.expenseType = ko.observable();
+    self.expenseCost = ko.observable();
+    self.taxrate = ko.observable();
+    self.tax = ko.computed(function() {
+      if(typeof self.expenseCost() != 'number' || typeof self.taxrate() != 'number') {
+        return null;
+      }
+      return (self.expenseCost() - self.expenseCost()/(1+self.taxrate()));
+    }, this);
+
+}
+
+    return model;
+  }
+)
